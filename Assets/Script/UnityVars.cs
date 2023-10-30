7d85abc4e090a82333c799f95ad8c728518ed286
saci zakaria <sacideveloppeur@gmail.com>
2023-10-30 19:29:54 +0100

    mat = rend.material;

--- Assets/Script/UnityVars.cs
+++ Assets/Script/UnityVars.cs
@@ -9,6 +9,7 @@ public class UnityVars : MonoBehaviour
     public Transform goTransform;
     public Vector3 goPos;
     public Renderer rend;
+    public Material mat;
     public Collider col;
 
     // Start is called before the first frame update
@@ -16,6 +17,8 @@ public class UnityVars : MonoBehaviour
     {
         goPos = goTransform.position;
         transform.position = goPos;
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
     }
 
     // Update is called once per frame
