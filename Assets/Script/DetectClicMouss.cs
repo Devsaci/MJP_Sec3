00d760251ff0d895b089f2dbad9c3cbd853e2699
saci zakaria <sacideveloppeur@gmail.com>
2023-11-02 10:28:59 +0100

    init DetectClicMouss

--- Assets/Script/DetectClicMouss.cs
+++ Assets/Script/DetectClicMouss.cs
@@ -6,21 +6,6 @@ public class DetectClicMouss : MonoBehaviour
 {
 
 
-    public Color[] colors;
-
-    private void OnMouseOver()
-    {
-        GetComponent<Renderer>().material.color = colors[0];
-    }
-
-    private void OnMouseExit()
-    {
-        GetComponent<Renderer>().material.color = colors[1];
-    }
-
-    private void OnMouseUp()
-    {
-        GetComponent<Renderer>().material.color = colors[2];
-    }
+ 
 
 }
