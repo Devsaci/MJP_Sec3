b0a86342b31c4812327c9b58bbbb3ac8711f90c4
saci zakaria <sacideveloppeur@gmail.com>
2023-11-05 22:02:13 +0100

    factoy Code

--- Assets/Script/DetectKeyBoard.cs
+++ Assets/Script/DetectKeyBoard.cs
@@ -6,32 +6,32 @@ public class DetectKeyBoard : MonoBehaviour
 {
 
 
-    public float speed = 100;
+    public float speed;
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow))
-        {
+
             MoveInDirection(Vector3.forward);
-        }
+
         if (Input.GetKey(KeyCode.DownArrow))
-        {
+
             MoveInDirection(Vector3.back);
-        }
+
         if (Input.GetKey(KeyCode.RightArrow))
-        {
+
             MoveInDirection(Vector3.right);
-        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
-        {
+
             MoveInDirection(Vector3.left);
-        }
+
 
     }
 
 
     public void MoveInDirection(Vector3 dir)
     {
-       transform.Translate(dir * Time.deltaTime * speed);   
+        transform.Translate(dir * Time.deltaTime * speed);
     }
 
 }
