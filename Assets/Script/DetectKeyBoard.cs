a61dab39a1ca9a83399330c9ff3c0536431565ad
saci zakaria <sacideveloppeur@gmail.com>
2023-11-05 20:47:39 +0100

    transform.Rotate(Vector3.right * 15 * Time.deltaTime);

--- Assets/Script/DetectKeyBoard.cs
+++ Assets/Script/DetectKeyBoard.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DetectKeyBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ 
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        transform.Rotate(Vector3.right * 15 * Time.deltaTime);
     }
 }
