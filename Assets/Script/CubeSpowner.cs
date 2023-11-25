f10499de50b2395b59bae34781bbcf7e59de848e
saci zakaria <sacideveloppeur@gmail.com>
2023-11-25 09:01:16 +0100

    float zval = Random.Range(-4.2f, 4.2f);

--- Assets/Script/CubeSpowner.cs
+++ Assets/Script/CubeSpowner.cs
@@ -16,6 +16,7 @@ public class CubeSpowner : MonoBehaviour
     public void CreatCube()
     {
         float xval = Random.Range(-4.2f, 4.2f);
+        float zval = Random.Range(-4.2f, 4.2f);
         Instantiate(cubePrefab, new Vector3(),Quaternion.identity);
     }
    
