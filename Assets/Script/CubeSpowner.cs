10e3193571acb85d9723f1ab8ef7ec9d03f46dc8
saci zakaria <sacideveloppeur@gmail.com>
2023-11-25 08:51:13 +0100

    public GameObject cubePrefab;

--- Assets/Script/CubeSpowner.cs
+++ Assets/Script/CubeSpowner.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CubeSpowner : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject cubePrefab;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
